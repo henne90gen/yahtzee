0e2a1bfaee6d41fad9ca177bc585238f8f53faf6
Hendrik Müller <henne90gen@gmail.com>
2023-01-20 12:37:11 +0100

    Switched from Create React App to Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
